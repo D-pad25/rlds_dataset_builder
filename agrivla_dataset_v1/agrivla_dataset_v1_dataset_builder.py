db955bac8e066f85b68d6fc0a320e70cb1e264f9
D-pad25 <daniel.padbury@gmail.com>
2025-05-24 17:43:44 +1000

    changed path

--- agrivla_dataset_v1/agrivla_dataset_v1_dataset_builder.py
+++ agrivla_dataset_v1/agrivla_dataset_v1_dataset_builder.py
@@ -100,7 +100,7 @@ class AgrivlaDatasetV1(tfds.core.GeneratorBasedBuilder):
             # path='/mnt/e/VLA_data/CleanData/*'),
             # Test on E:\VLA_data\CleanData224\v5
             # E:\VLA_data\JoblibData224_Steps
-            path='/mnt/e/Round1_addedPrompt/testJobLib/*'),
+            path='/mnt/e/Round1_addedPrompt/joblib/*'),
             # 'val': self._generate_examples(path='data/val/episode_*.npy'),
         }
 
