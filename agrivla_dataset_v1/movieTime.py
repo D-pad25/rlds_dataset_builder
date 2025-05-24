29a04b3139f4d0c56dde4b8858f56381877464e4
D-pad25 <daniel.padbury@gmail.com>
2025-05-24 17:30:20 +1000

    changed path

--- agrivla_dataset_v1/movieTime.py
+++ agrivla_dataset_v1/movieTime.py
@@ -35,7 +35,7 @@ def render_frame(base_img, wrist_img, state, action, prompt):
     return combined
 
 def create_episode_video(output_path="/mnt/e/VLA_data/movie/testtfds.mp4", fps=12):
-    builder = AgrivlaDatasetV1(data_dir="/mnt/e/TFDS_data")
+    builder = AgrivlaDatasetV1(data_dir="/mnt/e/TFDS_data/addedPromp")
     builder.download_and_prepare()
     ds = builder.as_dataset(split="train")
     episode = next(iter(tfds.as_numpy(ds.take(1))))  # Just one episode
