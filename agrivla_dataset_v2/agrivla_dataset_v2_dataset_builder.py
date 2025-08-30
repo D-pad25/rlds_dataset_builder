2e9fbe64b520d92e362506a369a316d013de79f9
D-pad25 <daniel.padbury@gmail.com>
2025-08-30 20:32:36 +1000

    test_checks

--- agrivla_dataset_v2/agrivla_dataset_v2_dataset_builder.py
+++ agrivla_dataset_v2/agrivla_dataset_v2_dataset_builder.py
@@ -99,7 +99,7 @@ class AgrivlaDatasetV2(tfds.core.GeneratorBasedBuilder):
         return {
             'train': self._generate_examples(
             # path='/mnt/e/Round1_addedPrompt/pkl/*'),
-            path = '/mnt/d/Sem2/Round_1/test_tfds/*'),
+            path = '/mnt/d/Sem2/Round_1/test_checks/*'),
         }
 
     def _generate_examples(self, path) -> Iterator[Tuple[str, Any]]:
